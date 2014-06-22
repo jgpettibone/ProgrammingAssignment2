c846155457655473ad2906a7414de666464002e2
jgpettibone <jeanette.pettibone@gmail.com>
2014-06-22 14:43:29 -0700

    first implementation of cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## order to cache the results of the time-consuming inverse function the 
 ## first time it gets computed for a matrix.  All subsequent times that 
 ## the inverse of the matrix is needed, the cache can be accessed instead 
-## of needing to compute the inverse again.  This saves resources.  To do this,
+## of needing to compute the inverse again.  This saves resources.  To do this,x
 ## we need to create a special matrix that keeps track of this information.   
 
 
@@ -26,8 +26,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function returns the inverse matrix.  First it checks to 
+## see if the solution has already been solved and cached.  If so, it returns
+## the cached data.  Otherwise, it gets the data out of the matrix, solves for 
+## the inverse matrix, caches the inverse matrix, and then returns the result.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if (!is.null(inverse)) {
+      message('getting cached data')
+      return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
