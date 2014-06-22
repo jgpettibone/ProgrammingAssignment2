7fa0d695fa290489b0d4db5616b62005cc78a69a
jgpettibone <jeanette.pettibone@gmail.com>
2014-06-22 15:00:51 -0700

    added more comments to explain usage

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,7 @@
 ## of needing to compute the inverse again.  This saves resources.  To do this,x
 ## we need to create a special matrix that keeps track of this information.   
 
+## Note: We are assuming that the matrix supplied is always invertible.
 
 ## The makeCacheMatrix function creates a special matrix which is really a 
 ## list containing functions that 
@@ -12,6 +13,13 @@
 ## 2. get the value of the matrix
 ## 3. set the inverse of the matrix
 ## 4. get the inverse of the matrix
+
+## To use:
+## 1. Create a matrix using the makeCacheMatrix function
+## > mat <- makeCacheMatrix()
+## 2. Set it's value using $set
+## > mat$set(matrix(1:4,2,2))
+## Now you have a matrix that is ready to be cached
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
@@ -31,6 +39,9 @@ makeCacheMatrix <- function(x = matrix()) {
 ## the cached data.  Otherwise, it gets the data out of the matrix, solves for 
 ## the inverse matrix, caches the inverse matrix, and then returns the result.
 
+## To use:
+## 1. call the cacheSolve function with your special matrix as the parameter
+## > cacheSolve(mat)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
